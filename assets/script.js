c056426be765d5b3af5698e0519242309ee7bdc3
Cristian Mancilla <crmago@gmail.com>
2024-03-10 03:32:04 -0300

    correcion de codigo JS de formulario, se agrega no posibilidad de envio si no se completan los datos, se linkean los iconos con las pagina correspondientes, se agregan tooltips a los botones y se ajustan algunos estilos de la hoja CSS

--- assets/script.js
+++ assets/script.js
@@ -13,15 +13,20 @@ $(function () {
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    let respuesta = prompt('Mensaje enviado correctamente. ¿Te podemos ayudar en algo más? (Sí/No)');
-    if (respuesta && respuesta.toLowerCase() === 'si') {
+    let nombre = document.getElementById('exampleInputName1').value.trim();
+    let email = document.getElementById('exampleInputEmail1').value.trim();
+    let telefono = document.getElementById('exampleInputTelephone1').value.trim();
+    let mensaje = document.getElementById('exampleInputMensaje1').value.trim();
 
-        let respuesta2 = prompt('¿En qué más te podemos ayudar?');
-
-        alert('Gracias por tu respuesta. Nos pondremos en contacto contigo lo antes posible.');
+    if (nombre === '' || email === '' || telefono === '' || mensaje === '') {
+        alert('Por favor, complete todos los campos antes de enviar el formulario.');
     } else {
-
-        alert('Gracias por contactarnos. Nos pondremos en contacto contigo lo antes posible.');
+        let respuesta = prompt('Mensaje enviado correctamente. ¿Te podemos ayudar en algo más? (Sí/No)');
+        if (respuesta && respuesta.toLowerCase() === 'si') {
+            let respuesta2 = prompt('¿En qué más te podemos ayudar?');
+            alert('Gracias por tu respuesta. Nos pondremos en contacto contigo lo antes posible.');
+        } else {
+            alert('Gracias por contactarnos. Nos pondremos en contacto contigo lo antes posible.');
+        }
     }
-
 });
